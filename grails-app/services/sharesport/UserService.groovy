3690cdf543b55baa7898b42f6a91ce57326b45d8
fabien <fabien.rolfo@master-developpement-logiciel.fr>
2014-10-23 08:51:46 +0200

    #2 deplacement controller vers service

---
+++ grails-app/services/sharesport/UserService.groovy
@@ -0,0 +1,39 @@
+package sharesport
+
+import sharesport.User
+
+/**
+ * Created by fabien on 23/10/14.
+ */
+
+
+class UserService {
+
+    def inscriptionUser(Map params) {
+        String result = ""
+        String emailError = ""
+        String loginError = ""
+
+        User user = new User()
+        user.login = params.login
+        user.email = params.email
+        user.password = params.password
+
+        if(!user.validate()){
+            if(User.findByLogin(params.login)){
+                loginError = "Login déjà utilisé"
+            }
+            if(User.findByEmail(params.email)){
+                emailError = "Email déjà utilisé"
+            }
+
+            result = "false"
+        }
+        else{
+            user.save()
+            result = "true"
+        }
+
+        ['succeed': result, 'emailError': emailError, 'loginError': loginError]
+    }
+}
