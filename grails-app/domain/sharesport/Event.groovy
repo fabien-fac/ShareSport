6fecd8ed29baebdd935100d7836fcf22898b3453
fabien <fabien.rolfo@master-developpement-logiciel.fr>
2014-11-13 09:13:21 +0100

    amelioration class Event et ajout du service event pour recherche par critere

--- grails-app/domain/sharesport/Event.groovy
+++ grails-app/domain/sharesport/Event.groovy
@@ -5,11 +5,14 @@ class Event {
     Sport sport
     Timeline timeline
     Date begin_date
+    String titre
+    User auteur
 
-    static hasMany = [hastags:Hashtag]
+    static hasMany = [hashtags:Hashtag]
 
 
     static constraints = {
+        titre blank: false
     }
 
     static mapping = {
---
+++ grails-app/services/sharesport/EventService.groovy
@@ -0,0 +1,55 @@
+package sharesport
+
+import grails.transaction.Transactional
+
+@Transactional
+class EventService {
+
+    def getEventWithCriteria(Map params) {
+        String titreEvent = params.titre
+        String sportTitreEvent = params.sport
+        String hashtagEvent = params.hashtag
+        String auteurEvent = params.auteur
+
+        int offsetP
+        if (!params.offset) {
+            offsetP = 0
+        } else {
+            offsetP = Integer.valueOf(params.offset)
+        }
+
+        int maxRes
+        if (!params.max) {
+            maxRes = 0
+        } else {
+            maxRes = Integer.valueOf(params.max)
+        }
+
+        def criteria = Event.createCriteria()
+        def results = criteria.list (offset: offsetP, max: maxRes){
+            if(titreEvent){
+                ilike('titre', "%$titreEvent%")
+            }
+            if(sportTitreEvent){
+                sport{
+                    ilike('name', "%$sportTitreEvent%")
+                }
+            }
+            if(hashtagEvent){
+                hashtags{
+                    ilike('label', "%$hashtagEvent%")
+                }
+            }
+            if(auteurEvent){
+                auteur{
+                    ilike('username', "%$auteurEvent%")
+                }
+            }
+            if(params.sort && params.order){
+                order(params.sort, params.order)
+            }
+        }
+
+        results
+    }
+}
