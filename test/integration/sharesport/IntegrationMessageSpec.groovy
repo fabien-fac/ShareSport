117f613edf86b62cd1478b4ec7aa02ea1f85bd03
fabien <fabien.rolfo@master-developpement-logiciel.fr>
2014-11-14 16:59:49 +0100

    #16 ajout des tests d integration du service event

--- test/integration/sharesport/IntegrationMessageSpec.groovy
+++ test/integration/sharesport/IntegrationMessageSpec.groovy
@@ -21,11 +21,6 @@ class IntegrationMessageSpec extends Specification {
 
     void "test de la suppression en cascade"() {
 
-        given: "un message avec un editeur"
-        editor = new User(email: "test@test.fr", login: "test", password: "testtest")
-        date = new Date()
-        message = new Message(editor: editor, content: "voici un test", date: date)
 
-        when: ""
     }
 }
