089174c963cccaca2b13022866665cb8bc22767f
akana <mao.akana@gmail.com>
2014-12-08 23:36:23 +0100

    creer classe de test d'integration de service de message

---
+++ test/integration/sharesport/IntegrationMessageServiceSpec.groovy
@@ -0,0 +1,25 @@
+package sharesport
+
+
+
+import spock.lang.*
+
+/**
+ *
+ */
+class IntegrationMessageServiceSpec extends Specification {
+
+    User editor
+    Message message
+    Date date
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test de la suppression en cascade"() {
+
+    }
+}
